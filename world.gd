0e20c0963a9dc41515649d5c3db974aedcbe7c6a
Susan Benjamin <sb@slightwit.ch>
2023-08-03 18:45:32 +0100

    Initial commit.

---
+++ world.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+
+func _unhandled_input(event):
+  if event.is_action("ui_cancel"):
+    get_tree().quit()
+
+
+func _process(delta):
+  $Box.position = Vector3(0, 1, sin(Time.get_ticks_msec()/500.0)*2.5)
+  $Camera.look_at($Box.position, Vector3.UP)

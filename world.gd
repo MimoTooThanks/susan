5d2a0b75aaeacc2f5b0d6be3487cfb93784af04f
MimoTooThanks <46490625+MimoTooThanks@users.noreply.github.com>
2023-08-03 22:32:52 +0100

    poo

--- world.gd
+++ world.gd
@@ -1,14 +1,21 @@
 extends Node3D
 
+var im_a_noob := true
 
 func _unhandled_input(event):
-  if event.is_action("ui_cancel"):
-    get_tree().quit()
-
+	if event.is_action("ui_cancel"):
+		get_tree().quit()
 
 func _process(delta):
-  $Box.position = Vector3(0, 1, sin(Time.get_ticks_msec()/500.0)*2.5)
-  $Camera.look_at($Box.position, Vector3.UP)
-
-  if ($Camera/Ray.is_colliding()):
-    RenderingServer.global_shader_parameter_set('fader_position', $Camera/Ray.get_collision_point())
+	$Box.position = Vector3(2+cos(Time.get_ticks_msec()/1000.0)*4, 2, sin(Time.get_ticks_msec()/1000.0)*7)
+	$Camera.look_at($Box.position, Vector3.UP)
+	
+	if ($Camera/Ray.is_colliding()):
+		RenderingServer.global_shader_parameter_set('fader_position', $Camera/Ray.get_collision_point())
+		if(im_a_noob):
+			im_a_noob = false
+			$Wall/Show.play("show")
+	else:
+		if(!im_a_noob):
+			im_a_noob = true
+			$Wall/Show.play_backwards("show")

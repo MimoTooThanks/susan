e5f4798bdabaabbd40da3a941a46f7a06e11b2a5
Susan Benjamin <sb@slightwit.ch>
2023-08-03 19:18:02 +0100

    the rest of the owl

--- world.gd
+++ world.gd
@@ -9,3 +9,4 @@ func _unhandled_input(event):
 func _process(delta):
   $Box.position = Vector3(0, 1, sin(Time.get_ticks_msec()/500.0)*2.5)
   $Camera.look_at($Box.position, Vector3.UP)
+  RenderingServer.global_shader_parameter_set('fader_position', $Box.position)
